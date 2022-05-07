c4bba8159d981b690d287fd46ea0256e4e8f97d5
DviK2 <dvikdvik@gmail.com>
2022-05-07 16:41:39 +0900

    add csv parser (#3)

--- FileUploader/FileUploader.WebService/Program.cs
+++ FileUploader/FileUploader.WebService/Program.cs
@@ -1,7 +1,11 @@
+using FileUploader.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddTransient<ICsvParser, CsvParser>();
 
 var app = builder.Build();
 
